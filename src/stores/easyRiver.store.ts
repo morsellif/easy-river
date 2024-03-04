a8f88759062066da7f7ad5d21ee50845f15f58b1
Francesco Morselli <francesco.morselli@me.com>
2024-03-04 22:53:58 +0100

    To avoid the flag not working... had to put the boolean value inside an array. Changed `Ref<T>` to `RemovableRef<T>`.

--- src/stores/easyRiver.store.ts
+++ src/stores/easyRiver.store.ts
@@ -1,21 +1,30 @@
 import { defineStore } from 'pinia';
+import { useLocalStorage, type RemovableRef } from '@vueuse/core';
 
 interface State {
-  bookmarks: Ref<Bookmark[]>;
-  bookmarkFilter: Ref<boolean>;
+  bookmarks: RemovableRef<Bookmark[]>;
+  bookmarkFilter: RemovableRef<boolean[]>;
 }
+
+const _bookmarksKey = 'easyRiver/bookmarks';
+const _bookmarkFilterKey = 'easyRiver/bookmarkFilter';
 
 export const useEasyRiverStore = defineStore('easyRiver', {
   state: (): State => ({
-    bookmarks: useLocalStorage<Bookmark[]>('easyRiver/bookmarks', []),
-    bookmarkFilter: useLocalStorage<boolean>('easyRiver/bookmarkFilter', false),
+    bookmarks: useLocalStorage<Bookmark[]>(_bookmarksKey, []),
+    bookmarkFilter: useLocalStorage(_bookmarkFilterKey, [false]),
   }),
+  hydrate(state, _initialState) {
+    state.bookmarks = useLocalStorage<Bookmark[]>(_bookmarksKey, []).value;
+    state.bookmarkFilter = useLocalStorage(_bookmarkFilterKey, [false]).value;
+  },
   getters: {
     isBookmarked: (state) => {
       return (idstazione: string) =>
         state.bookmarks.find((bookmark) => bookmark.idstazione === idstazione);
     },
     bookmarkCount: (state) => state.bookmarks.length,
+    filterBookmarks: (state) => state.bookmarkFilter[0],
   },
   actions: {
     addBookmark(idstazione: string) {
@@ -29,7 +38,7 @@ export const useEasyRiverStore = defineStore('easyRiver', {
       this.bookmarks.splice(indexToDelete, 1);
     },
     toggleBookmarkFilter() {
-      this.bookmarkFilter = !this.bookmarkFilter;
+      this.bookmarkFilter[0] = !this.bookmarkFilter[0];
     },
   },
 });
