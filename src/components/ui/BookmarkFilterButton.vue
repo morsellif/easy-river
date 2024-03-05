461686ae1538eaca13cf97834a704b266c1bc064
Francesco Morselli <francesco.morselli@me.com>
2024-03-05 08:34:36 +0100

    Added styling for hover: active: and filter active.

--- src/components/ui/BookmarkFilterButton.vue
+++ src/components/ui/BookmarkFilterButton.vue
@@ -5,7 +5,8 @@ const { filterBookmarks } = storeToRefs(store);
 
 <template>
   <button
-    class="rounded-full border border-slate-200 bg-transparent px-3 py-1 hover:border-slate-300 hover:bg-slate-100"
+    class="rounded-full border border-slate-200 bg-transparent px-3 py-1 hover:border-slate-300 hover:bg-slate-100 active:border-slate-400 active:bg-slate-200"
+    :class="[filterBookmarks ? 'border-slate-400 bg-slate-200/70' : '']"
     @click="store.toggleBookmarkFilter()"
   >
     <span class="flex space-x-2">
