d979f9218363687cbea2d17b4494f427a955c6fd
Francesco Morselli <francesco.morselli@me.com>
2024-03-05 07:18:48 +0100

    Basic button to toggle bookmark filtering.

---
+++ src/components/ui/BookmarkFilterButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const store = useEasyRiverStore();
+const { filterBookmarks } = storeToRefs(store);
+</script>
+
+<template>
+  <button
+    class="rounded-full border border-slate-200 bg-transparent px-3 py-1 hover:border-slate-300 hover:bg-slate-100"
+    @click="store.toggleBookmarkFilter()"
+  >
+    <span class="flex space-x-2">
+      <div>
+        <IconsFilter v-if="!filterBookmarks" />
+        <IconsFilterDismiss v-else />
+      </div>
+      <div>Bookmarks</div>
+    </span>
+  </button>
+</template>
