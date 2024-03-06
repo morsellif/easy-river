f55282c439a93f089e04690107119e427ce7c886
Francesco Morselli <francesco.morselli@me.com>
2024-03-06 11:17:03 +0100

    To avoid sending request on every page load, instead create an api route to handle caching. If key with the calculated epoch does not exists, fetch data and save it to cache (Redis) and set key expiration to 30 minutes (check for new data every 30 minutes), if exists get data from cache.

--- src/server/api/stations.ts
+++ src/server/api/stations.ts
@@ -1,24 +1,58 @@
-const backInTime = () => {
-  const currentDate = new Date();
-  currentDate.setHours(currentDate.getHours() - 1);
-  currentDate.setMinutes(0);
-  currentDate.setSeconds(0);
-  currentDate.setMilliseconds(0);
+import { addHours } from 'date-fns';
 
-  return currentDate;
+import { redis, keyParser, isCaching } from './../../utils/redis';
+
+const currentHourUTCEpoch = () => {
+  const currentDate = new Date();
+  if (currentDate.getMinutes() <= 30) {
+    currentDate.setMinutes(30, 0, 0);
+  } else {
+    addHours(currentDate, 1);
+    currentDate.setMinutes(0, 0, 0);
+  }
+
+  return currentDate.getTime();
 };
 
 export default defineEventHandler(async (_event) => {
-  const data = await $fetch<NoTimeResponse>(
-    'https://allertameteo.regione.emilia-romagna.it/o/api/allerta/get-sensor-values-no-time',
-    {
-      method: 'GET',
-      query: {
-        variabile: '254,0,0/1,-,-,-/B13215',
-        time: backInTime().getTime(),
-      },
-    },
-  );
+  // get the latest most complete stations data
+  const epoch = currentHourUTCEpoch().toString();
 
-  return data;
+  const fromCache = isCaching()
+    ? await redis.get<string>(keyParser(epoch))
+    : null;
+  if (!fromCache) {
+    console.log('Data not found in cache...');
+    console.log('Fetching new data...');
+    const data = await $fetch<NoTimeResponse>(
+      'https://allertameteo.regione.emilia-romagna.it/o/api/allerta/get-sensor-values-no-time',
+      {
+        method: 'POST',
+        query: {
+          variabile: '254,0,0/1,-,-,-/B13215',
+          time: epoch,
+        },
+        params: {
+          ts: epoch,
+          latestAvailabelTimeIsKnown: 'false',
+          time: epoch,
+        },
+      },
+    );
+
+    if (isCaching()) {
+      // using transaction to optimize Redis server calls
+      const transaction = redis.multi();
+      transaction.set(keyParser(epoch), JSON.stringify(data));
+      transaction.expire(keyParser(epoch), 30 * 60);
+
+      console.log('Saving new data to cache...');
+      const _result = await transaction.exec();
+    }
+
+    return data;
+  } else {
+    console.log('Data found in cache...');
+    return fromCache;
+  }
 });
